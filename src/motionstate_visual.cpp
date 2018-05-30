034cf5c664e1a8ec5a62d45b1c6742b6ca4d3959
Maximilian Naumann <maximilian.naumann@kit.edu>
2018-05-30 14:48:16 +0200

    Prepare for github-release: update LICENSE to BSD according to github-format use new split up utils

--- src/motionstate_visual.cpp
+++ src/motionstate_visual.cpp
@@ -1,11 +1,41 @@
+/*
+ * Copyright (c) 2017
+ * FZI Forschungszentrum Informatik, Karlsruhe, Germany (www.fzi.de)
+ * KIT, Institute of Measurement and Control, Karlsruhe, Germany (www.mrt.kit.edu)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <OGRE/OgreQuaternion.h>
 #include <OGRE/OgreSceneManager.h>
 #include <OGRE/OgreSceneNode.h>
 #include <OGRE/OgreVector3.h>
 
 #include <rviz/msg_conversions.h>
-#include <simulation_utils/util_rviz.hpp>
-#include <simulation_utils/util_rvizshapes.hpp>
+#include <util_rviz/util_rviz.hpp>
+#include <util_rviz/util_rvizshapes.hpp>
 #include <rviz/ogre_helpers/arrow.h>
 #include <rviz/ogre_helpers/movable_text.h>
 
@@ -43,11 +73,11 @@ MotionStateVisual::~MotionStateVisual() {
 
 // Position and orientation are passed through to the SceneNode.
 void MotionStateVisual::setFramePosition(const Ogre::Vector3& position) {
-    util_rviz::setSafePosition(frame_node_, position);
+    util_rviz::setPositionSafely(frame_node_, position);
 }
 
 void MotionStateVisual::setFrameOrientation(const Ogre::Quaternion& orientation) {
-    util_rviz::setSafeOrientation(frame_node_, orientation);
+    util_rviz::setOrientationSafely(frame_node_, orientation);
 }
 
 void MotionStateVisual::setVisible(const bool visible) {
@@ -67,7 +97,7 @@ void MotionStateVisual::setParams(const Parameters& p) {
 
 void MotionStateVisual::makeArrow(const automated_driving_msgs::MotionState& ms) {
 
-    if (!util_perception::twistValid(ms)) {
+    if (!util_automated_driving_msgs::checks::twistValid(ms)) {
         return;
     }
 
